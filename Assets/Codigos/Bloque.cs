edfd471c83bd3eafe27c9624a7c0212f4ada56ed
BruceMunizaga <brucemunizaga85@gmail.com>
2024-04-24 14:33:18 -0400

    feat(General): Se agrega el mapeado y funcionalidades generales
    Se añadió el mapeado de la escena en conjunto con los movimientos del personaje, movimientos de las monedas, y movimientos de los enemigos

---
+++ Assets/Codigos/Bloque.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class Bloque : MonoBehaviour
+{
+    [SerializeField] private Transform[] puntosMovimiento;
+    [SerializeField] private float velocidadMovimiento;
+    private int siguienteBloque = 1;
+    private bool ordenBloque = true;
+
+    private void Update(){
+        if(ordenBloque && siguienteBloque +1 >=puntosMovimiento.Length){
+            ordenBloque = false;
+        }
+        if(!ordenBloque && siguienteBloque <= 0){
+            ordenBloque = true;
+        }
+
+        if(Vector2.Distance(transform.position, puntosMovimiento[siguienteBloque].position) < 0.1f){
+            if(ordenBloque){
+                siguienteBloque += 1;
+            }else{
+                siguienteBloque -= 1;
+            }
+        }
+
+        transform.position = Vector2.MoveTowards(transform.position, puntosMovimiento[siguienteBloque].position, 
+        velocidadMovimiento * Time.deltaTime);
+    }
+}
