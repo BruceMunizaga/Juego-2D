79abe23997347a47cb0646843239e4dc8a0ce921
LuisCard3nas <130190271+LuisCard3nas@users.noreply.github.com>
2024-04-28 06:46:11 -0400

    a
    a

--- Assets/Codigos/Moneda.cs
+++ Assets/Codigos/Moneda.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class Moneda : MonoBehaviour
 {
+    private AudioSource audioSource;
+    [SerializeField] private AudioClip colectar1;
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
     }
 
     void OnTriggerEnter2D(Collider2D collision) {
     if (collision.CompareTag ("Player")) {
+        audioSource.PlayOneShot(colectar1);
         collision.gameObject.GetComponent<ColectorMonedas>().agarrar();
         Debug.Log("colectada la moneda");
     // El player ha tocado un enemigo
@@ -19,4 +22,5 @@ public class Moneda : MonoBehaviour
         }
 
     }
+
 }
